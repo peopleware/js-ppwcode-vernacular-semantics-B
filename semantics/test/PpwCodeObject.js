1b01cd2a4a7f3233a69bbb786d3e071c7798db8d
Jan Dockx <Jan_Dockx@peopleware.be>
2014-02-28 10:54:06 +0100

    split ppwcodeobject text for reuse

--- semantics/test/PpwCodeObject.js
+++ semantics/test/PpwCodeObject.js
@@ -1,40 +1,14 @@
-define(["../_util/contracts/doh", "../PpwCodeObject", "dojo/_base/declare", "module"],
-  function (doh, PpwCodeObject, declare, module) {
+define(["../_util/contracts/doh", "./PpwCodeObjectTestDefinition"],
+  function (doh, tests) {
 
-    var PpwCodeObjectStub1 = declare([PpwCodeObject], {});
-    PpwCodeObjectStub1.mid = module.id + "_PpwCodeObjectStub1";
-
-    var PpwCodeObjectStub2 = declare([PpwCodeObject], {}); // no mid
-
-    function test_getTypeDescription(/*Function*/ Constructor) {
-      var subject = new Constructor();
-      var result = subject.getTypeDescription();
-      doh.validateInvariants(subject);
-      // postconditions
-      doh.t(!!result);
-      doh.t(typeof result === "string");
-    }
-
-    doh.register(PpwCodeObject.mid, [
+    doh.register(tests.subjectMid, [
       {
         name: "Constructor test", // don't name a test "constructor"; it kills everything :-(
         runTest: function () {
-          var subject = new PpwCodeObjectStub1();
+          var subject = new tests.PpwCodeObjectStub1();
           doh.validateInvariants(subject);
         }
-      },
-      {
-        name: "getTypeDescription with mid",
-        runTest: function () {
-          test_getTypeDescription(PpwCodeObjectStub1);
-        }
-      },
-      {
-        name: "getTypeDescription with no mid",
-        runTest: function () {
-          test_getTypeDescription(PpwCodeObjectStub2);
-        }
       }
-    ]);
+    ].concat(tests));
   }
 );
