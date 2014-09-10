ab3145993307941fda716f76bceff3f4a01ba7e4
Jan Dockx <Jan_Dockx@peopleware.be>
2014-09-10 16:36:09 +0200

    dangling changes in README and intern config

--- README.md
+++ README.md
@@ -3,72 +3,32 @@ js-ppwcode-vernacular-semantics-private
 
 Getting started
 ---------------
-This project is defined  with submodules.
-To download the submodules, use the literal git commands:
+This project is a library. Apart from the tests, there is nothing to run here.
 
-    > git submodule init
-    > git submodule update
-
-This will download all needed submodules into `lib/`.
-
-
-
-
-
-Structure
----------
-The main project files, with their associated tests, are located in
-the root directory.
-
-`_util` and `_exceptions` contains coded needed for
-the main code, that will presumably appear in a separate project in
-a while.
+To use the library:
+* include it in your project
+* add it as a package with the correct location to dojoConfig.packages
 
 
 
 Dependencies
 ------------
-When using this library in your project, make sure to include these
-dependencies:
-
-* dojo/dojo
-* dojo/util
-
-dojo/util is only needed to run the unit tests.
+The library depends on *dojo* (http://dojotoolkit.org) via the usual package name
+`dojo`.
 
 
 
-Tests
------
-### DOH
-When the submodules are downloaded, the DOH tests should work out 
-of the box when you navigate your browser to
-`_tests/_doh/runTest.html`.
-Make sure to run the tests on a web server.
+Structure
+---------
+The main project files are located in the root directory.
 
-### Intern
-When the submodules are downloaded, the Intern tests should work out 
-of the box when you navigate your browser to
-`_tests/_intern/runTest.html`.
-Make sure to run the tests on a web server.
+`_util` and `_exceptions` contains coded needed for
+the main code, that will presumably appear in a separate project in
+a while.
 
-The tests can also be run using NodeJS.
-
-#### Setup:
-* install node on your system from <http://nodejs.org>
-* execute in this directory:
-
-    >\> npm install intern --save-dev
-
-
-This will install the necessary node modules in `./node-modules`.
-
-#### Run
-To run the tests, execute the following command in the root 
-directory: 
-
-    > node node_modules/intern/client config=_test/_intern/intern
-
+`_contract` contains contracts for the classes in the package.
+`_test` contains the test case factories, and `_test/_intern`
+contains the test setup for *intern* (http://theintern.io).
 
 
 
@@ -76,4 +36,40 @@ Demo
 ----
 The accompanying demo application can be found on
 <https://github.com/peopleware/js-ppwcode-enumeration-demo>.
+
+
+
+Tests
+-----
+Tests are generated automatically for a given test framework.
+Test setup is done assuming the dependent libaries are next to this library:
+* `../dojo`
+
+Setup is provided for *intern* (http://theintern.io).
+
+### Intern
+
+#### Setup:
+* install node on your system from <http://nodejs.org>
+* `cwd` should be 2 directories up (`../../`). Suppose the intermediate directory is called `<DIR>`.
+* execute (in `../../`):
+
+    >\> npm install intern --save-dev
+
+
+This will install the necessary node modules in `../../node-modules`.
+
+#### Browser Tests:
+he Intern tests should work when you navigate your browser to
+`_tests/_intern/runTest.html`.
+Make sure to run the tests on a web server.
+
+
+#### Run
+The tests can also be run using NodeJS.
+
+To run the tests, execute the following command in `../../`
+directory: 
+
+    > node node_modules/intern/client config=<DIR>/js-ppwcode-vernacular-semantics/_test/_intern/intern
 
--- _test/_intern/intern.js
+++ _test/_intern/intern.js
@@ -18,112 +18,122 @@
 // These default settings work OK for most people. The options that *must* be changed below are the
 // packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites.
 //noinspection MagicNumberJS
-define({
-	// The port on which the instrumenting proxy will listen
-	proxyPort: 9000,
+define(["intern", "module"], function(intern, module) {
 
-	// A fully qualified URL to the Intern proxy
-	proxyUrl: "http://localhost:9000/",
+  var x = 6;
 
-	// Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
-	// specified browser environments in the `environments` array below as well. See
-	// https://code.google.com/p/selenium/wiki/DesiredCapabilities for standard Selenium capabilities and
-	// https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
-	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
-	// automatically
-	capabilities: {
-		"selenium-version": "2.39.0"
-	},
+  console.log("TEST");
 
-	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
-	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
-	// capabilities options specified for an environment will be copied as-is
-	environments: [
-		{
-      browserName: "chrome",
-      version: "33",
-      platform: ["Windows 8.1", "Windows 8", "Windows 7", "OS X 10.9", "OS X 10.8", "Linux"]
+  return {
+
+    // The port on which the instrumenting proxy will listen
+    proxyPort: 9000,
+
+    // A fully qualified URL to the Intern proxy
+    proxyUrl: "http://localhost:9000/",
+
+    // Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
+    // specified browser environments in the `environments` array below as well. See
+    // https://code.google.com/p/selenium/wiki/DesiredCapabilities for standard Selenium capabilities and
+    // https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
+    // Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
+    // automatically
+    capabilities: {
+      "selenium-version": "2.39.0"
     },
-    {
-      browserName: "internet explorer",
-      version: "11",
-      platform: ["Windows 8.1", "Windows 7"]
+
+    // Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
+    // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
+    // capabilities options specified for an environment will be copied as-is
+    environments: [
+      {
+        browserName: "chrome",
+        version: "33",
+        platform: ["Windows 8.1", "Windows 8", "Windows 7", "OS X 10.9", "OS X 10.8", "Linux"]
+      },
+      {
+        browserName: "internet explorer",
+        version: "11",
+        platform: ["Windows 8.1", "Windows 7"]
+      },
+      {
+        browserName: "internet explorer",
+        version: "10",
+        platform: ["Windows 8", "Windows 7"]
+      },
+      {
+        browserName: "internet explorer",
+        version: "9",
+        platform: ["Windows 7"]
+      },
+      {
+        browserName: "firefox",
+        version: "28",
+        platform: [/*"Windows 8.1", "Windows 8", "Windows 7",*/ "OS X 10.9", "OS X 10.6", "Linux"]
+      },
+      {
+        browserName: "iphone",
+        version: "7.1",
+        platform: ["OS X 10.9"]
+      },
+      {
+        browserName: "ipad",
+        version: "7.1",
+        platform: ["OS X 10.9"]
+      },
+      {
+        browserName: "android",
+        version: "4.3",
+        platform: ["Linux"],
+        "device-type": "tablet"
+      },
+      {
+        browserName: "android",
+        version: "4.3",
+        platform: ["Linux"]
+      }
+    ],
+
+    // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
+    maxConcurrency: 2,
+
+    // Whether or not to start Sauce Connect before running tests
+    useSauceConnect: true,
+
+    // Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
+    // in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
+    // publishing this configuration file somewhere
+    webdriver: {
+      host: "localhost",
+      port: 4444
     },
-    {
-      browserName: "internet explorer",
-      version: "10",
-      platform: ["Windows 8", "Windows 7"]
+
+    // The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
+    // loader
+    useLoader: {
+      //		"host-node": "dojo/dojo",
+      //		"host-browser": "../../lib/dojo/dojo/dojo"
     },
-    {
-      browserName: "internet explorer",
-      version: "9",
-      platform: ["Windows 7"]
+
+    // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
+    // can be used here
+    loader: {
+      // Packages that should be registered with the loader in each testing environment
+      baseUrl: "",
+      packages: [
+        {name: "dojo", location: "../../../../lib/dojo/dojo"},
+        {name: "ppwcode-vernacular-semantics", location: "../../../../"}
+      ]
     },
-    {
-      browserName: "firefox",
-      version: "28",
-      platform: [/*"Windows 8.1", "Windows 8", "Windows 7",*/ "OS X 10.9", "OS X 10.6", "Linux"]
-    },
-    {
-      browserName: "iphone",
-      version: "7.1",
-      platform: ["OS X 10.9"]
-    },
-    {
-      browserName: "ipad",
-      version: "7.1",
-      platform: ["OS X 10.9"]
-    },
-    {
-      browserName: "android",
-      version: "4.3",
-      platform: ["Linux"],
-      "device-type": "tablet"
-    },
-    {
-      browserName: "android",
-      version: "4.3",
-      platform: ["Linux"]
-    }
-	],
 
-	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
-	maxConcurrency: 2,
+    // Non-functional test suite(s) to run in each browser
+    suites: ["_test/_intern/all"],
 
-	// Whether or not to start Sauce Connect before running tests
-	useSauceConnect: true,
+    // Functional test suite(s) to run in each browser once non-functional tests are completed
+    functionalSuites: [/* "myPackage/tests/functional" */],
 
-	// Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
-	// in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
-	// publishing this configuration file somewhere
-	webdriver: {
-		host: "localhost",
-		port: 4444
-	},
+    // A regular expression matching URLs to files that should not be included in code coverage analysis
+    excludeInstrumentation: /^node_modules\/|^lib\/|^_util\/|^_exceptions\/|^_test\/_intern\//
+  }
 
-	// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
-	// loader
-	useLoader: {
-//		"host-node": "dojo/dojo",
-//		"host-browser": "../../lib/dojo/dojo/dojo"
-	},
-
-	// Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
-	// can be used here
-	loader: {
-		// Packages that should be registered with the loader in each testing environment
-		packages: [
-      {name: "dojo",   location: "../../../../lib/dojo/dojo"},
-      {name: "ppwcode-vernacular-semantics",   location: "../../../../"}
-    ]
-	},
-
-	// Non-functional test suite(s) to run in each browser
-	suites: ["_test/_intern/all"],
-
-	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ /* "myPackage/tests/functional" */ ],
-
-	// A regular expression matching URLs to files that should not be included in code coverage analysis
-	excludeInstrumentation: /^node_modules\/|^lib\/|^_util\/|^_exceptions\/|^_test\/_intern\//
 });
